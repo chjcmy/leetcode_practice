87a49f490cd703d3e92ac74a17579f6159540c63
choeseonghyeon <chjcmy@naver.com>
2025-09-02 14:33:35 +0900

    feat: Implement merge-sorted-array
    정렬된 배열 병합 문제(Merge Sorted Array)의 merge 함수를 구현했습니다.
    두 포인터 방식을 사용하여 두 정렬된 배열을 제자리에서 병합합니다.

--- src/solve/easy/merge-sorted-array.ts
+++ src/solve/easy/merge-sorted-array.ts
@@ -21,7 +21,26 @@
  */
 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    // 여기에 구현하세요
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p = m + n - 1;
+
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+
+    while (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2--;
+        p--;
+    }
 }
 
 // Test cases
