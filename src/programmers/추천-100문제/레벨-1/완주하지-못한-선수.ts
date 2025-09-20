406c6650a9bb39374f8a5de8ee7c18e65c936cc4
choeseonghyeon <chjcmy@naver.com>
2025-09-20 12:00:00 +0900

    feat: Solve Programmers 완주하지-못한-선수

--- src/programmers/추천-100문제/레벨-1/완주하지-못한-선수.ts
+++ src/programmers/추천-100문제/레벨-1/완주하지-못한-선수.ts
@@ -19,9 +19,23 @@
 */
 
 function solution(participant: string[], completion: string[]): string {
-  let answer = "";
+const map = new Map<string, number>();
   // 문제 풀이
-  return answer;
+    for (const name of participant) {
+        map.set(name, (map.get(name) || 0) + 1);
+    }
+
+    for (const name of completion) {
+        map.set(name, map.get(name)! - 1);
+    }
+
+    for (const [name, count] of map) {
+        if (count === 1) {
+            return name;
+        }
+    }
+    
+    return "";
 }
 
 // 예제 테스트
