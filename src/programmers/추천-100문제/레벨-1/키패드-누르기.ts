2468c3d61da34311f8de1eefcfb1300c5afa5dda
choeseonghyeon <chjcmy@naver.com>
2025-10-05 12:00:00 +0900

    feat: Solve Programmers 키패드-누르기

--- src/programmers/추천-100문제/레벨-1/키패드-누르기.ts
+++ src/programmers/추천-100문제/레벨-1/키패드-누르기.ts
@@ -5,9 +5,47 @@
  */
 
 function solution(numbers: number[], hand: string): string {
-  let answer = "";
-  // 문제 풀이
-  return answer;
+    let result = '';
+
+
+
+    let leftPos = [3, 0];
+    let rightPos = [3, 2];
+    
+    const keypad: {[key: number]: number[]} = {
+        1: [0,0], 2: [0,1], 3: [0,2],
+        4: [1,0], 5: [1,1], 6: [1,2],
+        7: [2,0], 8: [2,1], 9: [2,2], 0: [3,1]
+    };
+    
+    const getDistance = (pos1: number[], pos2: number[]): number => {
+        return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
+    };
+    
+    for (const num of numbers) {
+        const targetPos = keypad[num];
+        
+        if ([1,4,7].includes(num)) {
+            result += 'L';
+            leftPos = targetPos;
+        } else if ([3,6,9].includes(num)) {
+            result += 'R';
+            rightPos = targetPos;
+        } else {
+            const leftDist = getDistance(leftPos, targetPos);
+            const rightDist = getDistance(rightPos, targetPos);
+            
+            if (leftDist < rightDist || (leftDist === rightDist && hand === 'left')) {
+                result += 'L';
+                leftPos = targetPos;
+            } else {
+                result += 'R';
+                rightPos = targetPos;
+            }
+        }
+    }
+    
+    return result;
 }
 
 // 예제 테스트
