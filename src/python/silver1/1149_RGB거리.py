f42249158c0f64da85e4b08738d6fe1abbf69ccc
choeseonghyeon <chjcmy@naver.com>
2025-11-03 15:17:30 +0900

    feat: Mark 'RGB거리' as solved and fix solution typo
    - Updated README.md to reflect 'RGB거리' as solved.
    - Added 'velog_post_2025-11-03_RGB거리.md' to .gitignore.
    - Fixed typo in  initialization.

--- src/python/silver1/1149_RGB거리.py
+++ src/python/silver1/1149_RGB거리.py
@@ -1,15 +1,29 @@
 import sys
 
 def solution():
-    # 여기에 문제 풀이 코드를 작성하세요.
-    pass
+    N = int(sys.stdin.readline())
+
+    dp = [[0] * 3 for _ in range(N)]
+
+    costs = list(map(int, sys.stdin.readline().split()))
+
+    dp[0][0] = costs[0]
+    dp[0][1] = costs[1]
+    dp[0][2] = costs[2]
+
+    for i in range(1, N):
+        costs = list(map(int, sys.stdin.readline().split()))
+        
+        dp[i][0] = costs[0] + min(dp[i-1][1], dp[i-1][2])
+        
+        dp[i][1] = costs[1] + min(dp[i-1][0], dp[i-1][2])
+
+        dp[i][2] = costs[2] + min(dp[i-1][0], dp[i-1][1])
+    
+
+    return min(dp[N-1])
+
 
 if __name__ == "__main__":
-    # 입력을 처리하는 부분입니다.
-    # 문제에 따라 수정하여 사용하세요.
-    # 예: n = int(sys.stdin.readline())
-    
-    # solution() 함수를 호출하고 결과를 출력합니다.
-    # result = solution()
-    # print(result)
-    pass
+    result = solution()
+    print(result)
