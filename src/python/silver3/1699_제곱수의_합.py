83339df24075bca09aec33fe7f5845bf92e13446
UserK <chjcmy@naver.com>
2025-12-23 09:55:13 +0900

    Solve 1699 제곱수의 합

--- src/python/silver3/1699_제곱수의_합.py
+++ src/python/silver3/1699_제곱수의_합.py
@@ -1,15 +1,13 @@
 import sys
 
-def solution():
-    # 여기에 문제 풀이 코드를 작성하세요.
-    pass
+n = int(sys.stdin.readline())
 
-if __name__ == "__main__":
-    # 입력을 처리하는 부분입니다.
-    # 문제에 따라 수정하여 사용하세요.
-    # 예: n = int(sys.stdin.readline())
-    
-    # solution() 함수를 호출하고 결과를 출력합니다.
-    # result = solution()
-    # print(result)
-    pass
+dp = [i for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    j = 1
+    while j * j <= i:
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
+        j += 1
+
+print(dp[n])
